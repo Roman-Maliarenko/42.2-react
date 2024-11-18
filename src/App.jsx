33cd8ea14d0fcc4ddbc470e7f633905835433f90
Roman-Maliarenko <romamalarenko1@gmail.com>
2024-11-18 18:23:59 +0300

    add new component Feedback

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 // import Lesson02 from './lessons/Lesson02/Lesson02';
 // import Homework02 from './homeworks/Homework02/Homeork02';
 // import Lesson03 from './lessons/Lesson03/Lesson';
-import Homework03 from './homeworks/Homework03/Homework03';
+import Homework04 from './homeworks/Homework04/Homework04';
 
 function App() {
   return (
@@ -11,7 +11,8 @@ function App() {
       {/* <Lesson02 />
       <Homework02/> */}
       {/* <Lesson03 /> */}
-      <Homework03 />
+      {/* <Homework03 /> */}
+      <Homework04 />
   </div>
   );
 }
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,15 +1,16 @@
-import './style.css'
+import "./style.css";
 
+function Button({ onClick, buttonName, type = "button" }) {
+  return (
+    <button className="main-button" type={type} onClick={onClick}>
+      {buttonName}
+    </button>
 
-function Button ({buttonName = 'Login'}){    
-    return (
-    <button className="main-button" type='button'>{buttonName}</button>
-
-// function Button (){
-//     const isGetButton = true;
-//     return (
-//     <button className="main-button">{isGetButton ? 'Get' : 'Send'}</button>
-
-)};
+    // function Button (){
+    //     const isGetButton = true;
+    //     return (
+    //     <button className="main-button">{isGetButton ? 'Get' : 'Send'}</button>
+  );
+}
 
 export default Button;
